ea50809dbc07e3f99b7972f0c85c95d84dbd8835
Jeroen Brauns <brauns.jeroen@pm.me>
2020-05-28 13:09:04 +0200

    Auto stash before revert of "Update version.txt"

--- src/main.py
+++ src/main.py
@@ -138,7 +138,7 @@ class MainPage(QtWidgets.QMainWindow):
 
         # Pre-system checks
         logging.info(f'========{date_time}========')
-        self.new_version = ''
+        self.new_version = current_version
 
         if self.check_update_wdt():  # Check for update WDT
             self.infobox_update(f'v{self.new_version} is nu beschikbaar om te installeren.\n Wil je deze nu downloaden?')
